d209bef91a466fe75044e0404ef64a7a8d7f1d56
MUDDUSSIR AWAN <126598592+MUDDUSSIR-RAZA@users.noreply.github.com>
2024-07-10 20:41:30 +0500

    Update course.js

--- model/db/course.js
+++ model/db/course.js
@@ -5,7 +5,7 @@ const courseSchema = new Schema({
     _id: { type: Types.ObjectId, default: () => new Types.ObjectId() },
     course_name: { type: String, required: true },
     questions: [{ type: Types.ObjectId, ref: 'Question' }],
-    quizOpen: { type: Boolean, default: false }, // New field fo
+    quizOpen: { type: Boolean, default: false }, // New field f
 });
 
 const course = mongoose.model('Course', courseSchema);
