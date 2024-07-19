3e0e13c5d8af6fdfa6843505016a540817068cfc
MR Dev <muddussir1247@gmail.com>
2024-07-19 07:46:44 -0700

    Update user.js

--- model/db/user.js
+++ model/db/user.js
@@ -1,23 +1,64 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  userName: { type: String, required: [true, "First name is required."] },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    validate: {
-      validator: async function (email) {
-        const existingUser = await this.constructor.findOne({ email });
-        return !existingUser;
-      },
-      message: "Email already exists!",
-    },
-  },
-  password: { type: String, required:  [true, "Please Enter a Password."] }
+const courseSchema = new Schema({
+    course_name: { type: String, required: [true, "Course name is required."] },
+    batch: { type: String, required: [true, "Batch is required."] },
+    city: { type: String, required: [true, "City is required."] },
+    // roll_no: { type: Number, required: [true, "Roll number is required."] },
+    status: { type: String, enum: ['pending', 'enrolled', 'failed'], default: 'pending', required: [true, "Status is required."] }
 });
 
-const User = mongoose.model("User", userSchema);
 
+const userSchema = new Schema({
+    name: { type: String, required: [true, "Name is required."] },
+    fathername: { type: String, required: [true, "Father's name is required."] },
+    nic: { type: String, required: [true, "NIC is required."] },
+    password: { type: String, required: [true, "Please enter a password."] },
+    email: {
+        type: String,
+        unique: true,
+        required: [true, "Email is required."],
+        validate: {
+            validator: async function (email) {
+                const existingUser = await this.constructor.findOne({ email });
+                return !existingUser;
+            },
+            message: "Email already exists!"
+        }
+    },
+    phone: { type: String, required: [true, "Phone number is required."] },
+    courses: [courseSchema],
+    // results: [{ type: Types.ObjectId, ref: 'Result' }]
+});
+
+const rollNoCounterSchema = new Schema({
+    last_roll_no: { type: Number, default: 0 }
+});
+
+userSchema.pre('save', async function(next) {
+  const student = this;
+  if (!student.roll_no) {
+      let counter = await RollNoCounter.findOne();
+      if (!counter) {
+          counter = new RollNoCounter();
+      }
+      counter.last_roll_no += 1;
+      student.roll_no = counter.last_roll_no;
+      await counter.save();
+  }
+  for (let i = 0; i < student.courses.length; i++) {
+      if (!student.courses[i].roll_no) {
+          let counter = await RollNoCounter.findOne();
+          counter.last_roll_no += 1;
+          student.courses[i].roll_no = counter.last_roll_no;
+          await counter.save();
+      }
+  }
+  next();
+});
+
+
+const RollNoCounter = mongoose.model('RollNoCounter', rollNoCounterSchema);
+const User = mongoose.model("User", userSchema);
 module.exports = User;
