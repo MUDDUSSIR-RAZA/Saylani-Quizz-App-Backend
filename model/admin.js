43800f6fde1964b26d2fb91742789333e4f70fe8
MR Dev <muddussir1247@gmail.com>
2024-07-21 12:21:56 -0700

    Update admin.js

--- model/admin.js
+++ model/admin.js
@@ -1,11 +1,26 @@
 const User = require("./db/user");
 
-exports.studentRequestModel = async () => {
+exports.getStudentRequestModel = async () => {
     try {
-        const studentRequests = await User.find({ attest: "pending" }).select("-password")
-        return studentRequests;
+        const getStudentRequests = await User.find({ attest: "pending" }).select("-password")
+        return getStudentRequests;
     } catch (error) {
         console.error(error);
         throw new Error('Error retrieving student requests');
     }
 };
+
+exports.attestStudentRequestModel = async (id, status) => {
+    try {
+        const attest = {
+            attest: status
+        }
+        const result = await User.findOneAndUpdate({ _id: id }, attest, { new: true });
+        if (!result) {
+            throw "Student not Found!";
+        }
+        return "Name Successfully Updated!";
+    } catch (error) {
+        throw error
+    }
+}
