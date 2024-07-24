dddf7f46cbce6e704f69eacc7192061bf455d512
MR Dev <muddussir1247@gmail.com>
2024-07-24 05:35:01 -0700

    Update user.js

--- model/db/user.js
+++ model/db/user.js
@@ -13,7 +13,16 @@ const courseSchema = new Schema({
 const userSchema = new Schema({
     name: { type: String, required: [true, "Name is required."] },
     fathername: { type: String, required: [true, "Father's name is required."] },
-    nic: { type: String, required: [true, "NIC is required."] },
+    nic: {
+        type: String, unique: [true, "NIC already exists!"], required: [true, "NIC is required."],
+        validate: {
+            validator: async function (nic) {
+                const existingNIC = await this.constructor.findOne({ nic });
+                return !existingNIC;
+            },
+            message: "NIC already exists!"
+        }
+    },
     password: { type: String, required: [true, "Please enter a password."] },
     email: {
         type: String,
@@ -30,36 +39,37 @@ const userSchema = new Schema({
     attest: { type: String, enum: ['pending', 'verified', 'Unverified'], default: 'pending', required: [true, "Status is required."] },
     phone: { type: String, required: [true, "Phone number is required."] },
     courses: [courseSchema],
+    role: { type: String, default: 'student'},
     // results: [{ type: Types.ObjectId, ref: 'Result' }]
 });
 
-const rollNoCounterSchema = new Schema({
-    last_roll_no: { type: Number, default: 0 }
-});
+// const rollNoCounterSchema = new Schema({
+//     last_roll_no: { type: Number, default: 0 }
+// });
 
-userSchema.pre('save', async function(next) {
-  const student = this;
-  if (!student.roll_no) {
-      let counter = await RollNoCounter.findOne();
-      if (!counter) {
-          counter = new RollNoCounter();
-      }
-      counter.last_roll_no += 1;
-      student.roll_no = counter.last_roll_no;
-      await counter.save();
-  }
-  for (let i = 0; i < student.courses.length; i++) {
-      if (!student.courses[i].roll_no) {
-          let counter = await RollNoCounter.findOne();
-          counter.last_roll_no += 1;
-          student.courses[i].roll_no = counter.last_roll_no;
-          await counter.save();
-      }
-  }
-  next();
-});
+// userSchema.pre('save', async function (next) {
+//     const student = this;
+//     if (!student.roll_no) {
+//         let counter = await RollNoCounter.findOne();
+//         if (!counter) {
+//             counter = new RollNoCounter();
+//         }
+//         counter.last_roll_no += 1;
+//         student.roll_no = counter.last_roll_no;
+//         await counter.save();
+//     }
+//     for (let i = 0; i < student.courses.length; i++) {
+//         if (!student.courses[i].roll_no) {
+//             let counter = await RollNoCounter.findOne();
+//             counter.last_roll_no += 1;
+//             student.courses[i].roll_no = counter.last_roll_no;
+//             await counter.save();
+//         }
+//     }
+//     next();
+// });
 
 
-const RollNoCounter = mongoose.model('RollNoCounter', rollNoCounterSchema);
+// const RollNoCounter = mongoose.model('RollNoCounter', rollNoCounterSchema);
 const User = mongoose.model("User", userSchema);
 module.exports = User;
