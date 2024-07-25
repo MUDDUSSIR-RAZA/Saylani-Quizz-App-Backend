69721d2f0880c4252fbd70c490f61d37d3719be5
MR Dev <muddussir1247@gmail.com>
2024-07-25 11:17:36 -0700

    Update admin.js

--- model/admin.js
+++ model/admin.js
@@ -1,4 +1,4 @@
-const course = require("./db/course");
+const Course = require("./db/course");
 const User = require("./db/user");
 
 exports.getStudentRequestModel = async () => {
@@ -29,27 +29,42 @@ exports.attestStudentRequestModel = async (id, status) => {
 
 exports.addCourseModel = async (course_name, batch, cities) => {
     try {
-        const course = new course({
-            course_name, batch, cities
-        })
+        const course = new Course({ course_name, batch, cities });
+
         try {
             await course.save();
             return "Course Created!";
         } catch (error) {
             if (error.name === 'ValidationError') {
-                for (field in error.errors) {
+                console.log("🚀 ~ exports.addCourseModel= ~ ValidationError:", "ValidationError")
+                // Iterate through each field error
+                for (let field in error.errors) {
+                    console.log("🚀 ~ exports.addCourseModel= ~ error.errors[field].message:", error.errors[field].message)
                     throw (error.errors[field].message);
                 }
             }
             // Check for duplicate key error
             else if (error.code === 11000) {
-                throw ('NIC already exists!');
+                console.log("🚀 ~ exports.addCourseModel= ~ Course already exists!:", "Course already exists!")
+                throw ('Course already exists!');
             } else {
-                throw ('An unknown error occurred:', error);
+                throw('An unknown error occurred:', error);
             }
-
         }
     } catch (error) {
-        throw error
+        // Log the error and rethrow it to be handled by the caller
+        console.error("Error in addCourseModel:", error);
+        throw error;
     }
-}
+};
+
+
+exports.getCoursesModel = async () => {
+    try {
+        const getCourses = await Course.find()
+        return getCourses;
+    } catch (error) {
+        console.log("🚀 ~ exports.getCoursesModel= ~ error:", error)
+        throw ('Error retrieving student requests');
+    }
+};
