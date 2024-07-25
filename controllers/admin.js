a6eb034ae3d36d8538c526184053fbc80b479038
MR Dev <muddussir1247@gmail.com>
2024-07-25 11:17:25 -0700

    Update admin.js

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,5 @@
 const express = require(`express`);
-const { getStudentRequestModel, attestStudentRequestModel } = require("../model/admin");
+const { getStudentRequestModel, attestStudentRequestModel, addCourseModel, getCoursesModel } = require("../model/admin");
 
 exports.getStudentRequestsController = async () => {
     try {
@@ -10,11 +10,30 @@ exports.getStudentRequestsController = async () => {
     }
 }
 
-exports.attestStudentRequestController = async (id , status) => {
+exports.attestStudentRequestController = async (id, status) => {
     try {
-        const resp = await attestStudentRequestModel(id , status);
+        const resp = await attestStudentRequestModel(id, status);
         return resp;
     } catch (error) {
         throw error;
+    }
+}
+
+exports.addCourseController = async (course_name, batch, cities) => {
+    try {
+        const resp = await addCourseModel(course_name, batch, cities)
+        return resp
+    } catch (error) {
+        console.log("🚀 ~ exports.addCourseController= ~ error:", error)
+        throw error
+    }
+}
+
+exports.getCoursesController = async () => {
+    try {
+        const resp = await getCoursesModel();
+        return resp
+    } catch (error) {
+        throw err;
     }
 }
