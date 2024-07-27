d09416984a323b1f9c3f25ed249592e2747ad62b
MR Dev <muddussir1247@gmail.com>
2024-07-27 01:13:51 -0700

    Update question.js

--- model/db/question.js
+++ model/db/question.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 // Define the Question schema
 const questionSchema = new Schema({
-    quizId:{ type: Schema.Types.ObjectId, ref: "Quiz"},
+    quiz:{ type: Schema.Types.ObjectId, ref: "Quiz"},
     question_text: { type: String, required: [true, "Question required."] },
     options: { type: [String],  required: [true, "Options required."] },
     correct_answer: { type: String,  required: [true, "Correct Answer required."] },
