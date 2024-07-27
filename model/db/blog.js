55d5708a823876e36da45cc2893b95b6292196c6
MR Dev <muddussir1247@gmail.com>
2024-07-26 22:31:14 -0700

    Update blog.js

--- model/db/blog.js
+++ model/db/blog.js
@@ -3,12 +3,9 @@ const Schema = mongoose.Schema;
 
 const blogSchema = new Schema({
   author: { type: Schema.Types.ObjectId, ref: "User" },
-  title:  { type: String, required: [true, "Title is required."] },
-  description:  { type: String, required: [true, "Description is required."] },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
+  title: { type: String, required: [true, "Title is required."] },
+  description: { type: String, required: [true, "Description is required."] },
+  date: { type: Date, default: Date.now, },
 });
 
 const Blog = mongoose.model("Blog", blogSchema);
