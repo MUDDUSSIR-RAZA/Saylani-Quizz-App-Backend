465036a35f507002ddfec0a184be5fcbe7c9c976
MR Dev <muddussir1247@gmail.com>
2024-07-28 09:53:45 -0700

    Update student.js

--- controllers/student.js
+++ controllers/student.js
@@ -1,4 +1,4 @@
-const { getStudentQuizModel } = require('../model/student');
+const { getStudentQuizModel, getQuizByIdModel } = require('../model/student');
 
 jwt = require('jsonwebtoken');
 
@@ -15,9 +15,8 @@ exports.getStudentQuizController = async (token) => {
 exports.getQuizByIdController = async (token, quizId) => {
     try {
         const { userId } = await jwt.verify(token, process.env.SECRET_KEY);
-        console.log(userId , quizId)
-        // const resp = getStudentQuizModel(userId)
-        return "resp"
+        const resp = getQuizByIdModel(userId , quizId)
+        return resp
     } catch (error) {
         throw error
     }
