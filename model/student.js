b75b385fdc90b371e8d97c451d8eb8a55fd9af90
MR Dev <muddussir1247@gmail.com>
2024-07-29 07:54:19 -0700

    Update student.js

--- model/student.js
+++ model/student.js
@@ -6,30 +6,29 @@ const User = require("./db/user");
 exports.getStudentQuizModel = async (userId) => {
     try {
         const user = await User.findById(userId)
-
         if (!user) {
             throw ('User not found');
         }
         const enrolledCourses = user.courses.filter(course => course.status === 'enrolled');
-
+        
         if (enrolledCourses.length === 0) {
             throw ('User is not enrolled in any courses');
         }
-
+        
         const filterQuizzes = [];
-
+        
         for (const course of enrolledCourses) {
             const quizzes = await Quiz.find({
                 course_name: course.course_name,
                 quizOpen: true,
                 $expr: { $gte: [{ $size: "$questions" }, 10] }
             }).populate('questions');
-
+            
             quizzes.forEach(quiz => {
                 filterQuizzes.push(quiz);
             });
         }
-
+        
         return filterQuizzes;
     } catch (error) {
         throw error.message
