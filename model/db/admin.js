cda83d01f2c2eb1013c45ccd14f4f71128070563
MR Dev <muddussir1247@gmail.com>
2024-08-03 20:02:19 +0500

    Update admin.js

--- model/db/admin.js
+++ model/db/admin.js
@@ -1,23 +1,32 @@
 const mongoose = require('mongoose');
 
 const adminSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    default: 'admin',
-  },
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }, 
+    phone: {
+        type: String, required: [true, "NIC is required."],
+        validate: {
+            validator: function (v) {
+                return /^\d{11}$/.test(v);
+            },
+            message: `Enter a valid Number!`
+        }
+    },
+    role: {
+        type: String,
+        default: 'admin',
+    },
 });
 
 const Admin = mongoose.model('Admin', adminSchema);
