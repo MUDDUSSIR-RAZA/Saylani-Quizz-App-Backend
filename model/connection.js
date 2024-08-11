af8e065942a80727c9b0af492fb2da07312bc6d0
MR Dev <muddussir1247@gmail.com>
2024-08-11 22:37:37 +0500

    Update connection.js

--- model/connection.js
+++ model/connection.js
@@ -4,7 +4,8 @@ const url = "mongodb://127.0.0.1:27017/SMITQuizApp";
 
 exports.mongoose = async () => {
   try {
-    let connect = await mongoose.connect(process.env.MONGODB_URL);
+    let connect = await mongoose.connect(url);
+    // let connect = await mongoose.connect(process.env.MONGODB_URL);
     console.log("connected");
   } catch (err) {
     console.log(err)
