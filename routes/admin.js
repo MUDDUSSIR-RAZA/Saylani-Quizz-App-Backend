f7e8393a698992c037e0b459e2a38d55e74be447
MR Dev <muddussir1247@gmail.com>
2024-08-11 09:49:11 +0500

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -88,6 +88,7 @@ router.post("/deleteQuiz", async (req, res) => {
       const resp = await deleteQuizController(req.body._id);
       res.status(200).json(resp);
     } catch (err) {
+        console.log(err)
       res.status(400).json(err);
     }
   });
