a9d0726284c7b973464846e59929e0ddf24582c2
MR Dev <muddussir1247@gmail.com>
2024-08-16 00:43:31 +0500

    Update app.js

--- app.js
+++ app.js
@@ -19,21 +19,7 @@ app.use(bodyparser.json({ limit: '50mb' }));
 app.use(bodyparser.urlencoded({ extended: true, limit: '50mb' }));
 app.use(express.static(public));
 
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://saylani-quizz-app.vercel.app"
-];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true, // If you need to send cookies or other credentials
-};
+app.use(cors()); // This will allow all origins by default
 
 app.use(cors(corsOptions));
 
