90efab7b595c25175bf914c0c30d19bc85299508
MR Dev <muddussir1247@gmail.com>
2024-08-17 13:48:56 +0500

    Update student.js

--- routes/student.js
+++ routes/student.js
@@ -7,6 +7,7 @@ router.get("/getStudentQuiz", async (req, res) => {
     try {
         const token = req.query.token;
         const resp = await getStudentQuizController(token)
+        console.log("resp " , resp)
         res.status(200).json(resp)
     } catch (err) {
         res.status(404).json(err)
